fb54939e2aad46be1eac3016cdab3ff4b067cf1d
Smyja <akposlive59@gmail.com>
2021-08-13 10:40:43 +0000

    LEARNING GOLANG1

--- main.go
+++ main.go
@@ -1,14 +1,17 @@
 package main
-import fmt
+import "fmt"
 
 func main(){
+	// ":-" declares a variable
+	// [] is used for a slice(an array that contains the same type of elements)
+	
 	cards:= []string{"Ace of Diamonds",newCard()}
 	cards=append(cards,"Queens Knight")
-	for i , card:=range cards{
+	for i, card:=range cards{
 		fmt.Println(i,card)
 	}
 }
 
-func newCard() string{
+func newCard() string {
 	return "Five of Diamonds"
 }
